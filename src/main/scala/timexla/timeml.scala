190f50c14cfe43a7e3d6ae33121e2797d1f6e332
Christopher Brown <io@henrian.com>
2012-02-26 12:07:24 -0600

    Converted python timex parser to scala.

--- src/main/scala/timexla/timeml.scala
+++ src/main/scala/timexla/timeml.scala
@@ -200,7 +200,6 @@ case class Document(text: String, filename: String) {
   }.toList
 
   override def toString = {
-//    val colored_tokens = 
     tokens.zip(tags).map { case (token, tag) =>
       val color = tag match {
         case BIOTag.B => Console.GREEN
@@ -209,103 +208,11 @@ case class Document(text: String, filename: String) {
       }
       color + token
     }.mkString(" ") + Console.RESET
-    //tokens.mkString(" / ")
 //      println("%16s ".format(tokens(i)) + color + tags.mkString(" ") + )
 //    filename+"\n"+text+"\n"+created+spans.mkString("\n")+"\n"+zipped.mkString("\n")
   }
 }
 
-
-
-// <TLINK lid="l115" relType="BEFORE" eventInstanceID="ei2309" relatedToTime="t161"/>
-// <TLINK lid="l114" relType="AFTER" eventInstanceID="ei2374" relatedToEventInstance="ei2293"/>
-// <TLINK lid="l22" relType="SIMULTANEOUS" eventInstanceID="ei2290" relatedToEventInstance="ei2292" signalID="s311"/>
-// <SLINK lid="l94" relType="EVIDENTIAL" eventInstanceID="ei2337" subordinatedEventInstance="ei2338"/>
-// <ALINK lid="l101" relType="CONTINUES" eventInstanceID="ei2263" relatedToEventInstance="ei2264"/>
-
-//@spans = spans.select{ |span| !span.text.empty? }
-
-// case class Span(dict: Map[String, Any]) {
-//   val text = dict.get("text") match {
-//     case Some(x: String) => x
-//     case _ => ""
-//   }
-//   val begin = dict.get("begin") match {
-//     case Some(x: Int) => x
-//     case _ => -1
-//   }
-//   val end = dict.get("end") match {
-//     case Some(x: Int) => x
-//     case _ => -1
-//   }
-
-//   val range = (begin, end)
-
-//   override def toString = {
-//     text+" ["+begin+"-"+end+"]"
-//   }
-// }
-
-// object Docume2nt {
-  // def parse(text: String, filename: String) {
-    // twitter.Json.parse(text) match {
-      // case json_document: Map[String, Any] => {
-        // the get below can't be getOrElse, btw
-        // json_document.get("documents") match {
-          // case Some(document_list: List[Map[String, Any]]) =>
-            // document_list.map { document => Document(document) }
-          // case _ => {
-            // println("documents in JSON is not a list.")
-            // List[Document]()
-          // }
-        // }
-      // }
-      // case _ => {
-        // println("No documents in JSON.")
-        // List[Document]()
-      // }
-    // }
-//   }
-// }
-
-  // document = {
-  //     filename: filename_str.tml,
-  //     text: token1 token2 token3 ...,
-  //     tokens: [[0,10], [11,19], ...],
-  //     created: str,
-  //     spans: [ { text: str, begin: int, end: int }, ... ]
-  // }
-
-  // val filename = dict.get("filename") match {
-    // case Some(x: String) => x
-    // case _ => ""
-  // }
-  // val text = dict.get("text") match {
-    // case Some(x: String) => x
-    // case _ => ""
-  // }
-  // val sentences = text.split(" ?\\.+ ?").map(_.split(" ").toList)
-  // sentences.foreach(x => println("sent: "+x))
-
-  // val token_indices = dict.get("tokens") match {
-  //   case Some(token_list: List[List[Int]]) => {
-  //     token_list.map { tuple: List[Int] => (tuple(0), tuple(1)) }
-  //   }
-  //   case _ => List[(Int, Int)]()
-  // }
-
-
-  // val created = dict.get("created") match {
-  //   case Some(x: String) => x
-  //   case _ => ""
-  // }
-  // spans is the List[Span] of timex's in the document, by timex3 spec and begin/end indices,
-  //   where those indices are indices of the items in the tokens List[String]
-  // val spans = dict.get("spans") match {
-  //   // case Some(span_list: List[Map[String, Any]]) => span_list.map { tuple => Span(tuple) }
-  //   case Some(span_list: List[Map[String, Any]]) => span_list.map(Span)
-  //   case _ => List[Span]()
-  // }
 
   // List[BIOTag] enumerations
   // val tags = (0 to tokens.length).map { i =>
